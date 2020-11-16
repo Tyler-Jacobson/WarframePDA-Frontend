b49263d9de8d97452b78855fc165dd833bb3b1e9
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2020-11-16 14:23:15 -0800

    Set up base components

---
+++ src/Components/WarframeCard.js
@@ -0,0 +1,13 @@
+import react from "react";
+
+
+const WarframeCard = function(props) {
+    console.log("THIS IS PROPS", props);
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default WarframeCard;
