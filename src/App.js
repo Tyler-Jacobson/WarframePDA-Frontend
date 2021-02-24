3d2f61955b385f36808c04410becdba1f5df02b2
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2021-02-24 14:35:26 -0800

    restructured files and working on nav bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { Route, Switch } from 'react-router-dom';
 import './styles/App.css';
 
+import Nav from "./Components/Nav";
 import Home from "./Components/Home";
+import Items from "./Components/Items";
+import About from "./Components/About"
 
 
 function App() {
 
   return (
     <div className="App">
-      <Home />
+      <Nav />
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/items" component={Items}/>
+        <Route path="/about" component={About}/>
+      </Switch>
     </div>
   );
 }
