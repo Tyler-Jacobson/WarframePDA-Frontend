057f70ac949019b1bdebfa93e34aad22c17496f0
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2021-02-23 21:31:32 -0800

    all basic functionality working. time to deploy

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,8 +7,6 @@ import axios from "axios";
 const defaultItemsData = []
 
 const Home = function() {
-    // console.log("THIS IS PROPS", props);
-    
 
     const [ itemsData, setItemsData ] = useState(defaultItemsData);
 
@@ -16,16 +14,16 @@ const Home = function() {
     
   
     const fetchData = function() {
-        
+      setItemsData(defaultItemsData)
       axios.get("https://warframepda.herokuapp.com/items/all")
       .then((res) => {
         
-        console.log("res.data", res.data)
+        // console.log("res.data", res.data)
 
         const arrayWithDataParams = addDataParameters(res.data)
         const arraySortedByProfit = sortByProfit(arrayWithDataParams)
         setItemsData(arraySortedByProfit)
-        console.log("returned from sortByProfit", arraySortedByProfit)
+        // console.log("returned from sortByProfit", arraySortedByProfit)
 
       })
       .catch((err) => {
@@ -35,6 +33,7 @@ const Home = function() {
 
     useEffect(() => {
       fetchData();
+      // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const addDataParameters = function(incomingArray) {
@@ -53,21 +52,22 @@ const Home = function() {
             let setObject = {}
     
             partsArray.map(function(part) {
-                const testobject = {
-                    "sectionname" : part.partname,
+                const sectionObject = {
+                    "sectionname" : part.partname + ":",
                     "pricerange" : [part.orders[0].price, part.orders[part.orders.length - 1].price],
+                    "fulldata" : part.orders,
                     "dropdown" : true,
-                    "fulldata" : part.orders
+                    "extended": false
                 }
     
                 if (part.partname === "Set") {
-                    testobject.sectionname = "Gained From Sold Set:"
-                    setObject = testobject
+                    sectionObject.sectionname = "Gained From Sold Set:"
+                    setObject = sectionObject
                 } else {
-                    minTotalCost += testobject.pricerange[0]
-                    maxTotalCost += testobject.pricerange[1]
+                    minTotalCost += sectionObject.pricerange[0]
+                    maxTotalCost += sectionObject.pricerange[1]
     
-                    displayArray.push(testobject)
+                    displayArray.push(sectionObject)
                 } 
                 return null; // this does nothing, it's just to get rid of the warning
             })
@@ -93,7 +93,7 @@ const Home = function() {
 
             // takes the original incoming item and replaces its 'parts' array with the newly created one
             const outgoingItem = incomingItem
-            outgoingItem.displayData = displayArray
+            outgoingItem.parts = displayArray
 
             fullDataArray.push(outgoingItem)
 
@@ -105,24 +105,87 @@ const Home = function() {
     const sortByProfit = function(incomingArray) {
         
         const sortedArray = incomingArray.sort((a, b) => {
-            return b.displayData[6].pricerange[0] - a.displayData[6].pricerange[0];
+            return b.parts[6].pricerange[0] - a.parts[6].pricerange[0];
         });
 
         return sortedArray
     }
 
+    const renderSection = function(itemIndex, partIndex) {
+        const tempItemsData = [...itemsData]
+        tempItemsData[itemIndex].parts[partIndex].extended = !tempItemsData[itemIndex].parts[partIndex].extended
+        setItemsData(tempItemsData)
+    }
+
+    const handleFocus = function(event) {
+        return event.target.select()
+    }
+
+    const renderExtendedData = function(section, item, partIndex) {
+        return (
+            <>
+                {
+                    section.fulldata.map(function(order) {
+                        // console.log(item.parts)
+                        if (partIndex <= 3){
+                            return (
+                                // <p className="extended-line" key={order.orderid}>/w {order.seller} Hi! I want to buy: {item.itemname} Prime {item.parts[partIndex].sectionname} for {order.price} platinum. (warframe.market)</p>
+                                <input className="extended-line" key={order.orderid} onFocus={handleFocus} readOnly value={`/w ${order.seller} Hi! I want to buy: ${item.itemname} Prime ${item.parts[partIndex].sectionname} for ${order.price} platinum. (warframe.market)`}/>
+                            
+                            
+                                )
+                        } else {
+                            return (
+                                <input className="extended-line" key={order.orderid} readOnly value={`${order.seller} is selling ${item.itemname} Prime Set for ${order.price} platinum`} />
+                            )
+                        }
+                        
+                    })
+                }
+            </>
+        )
+    }
+
     const renderData = function() {
         return (
-            <div>
+            <div className="home">
                 {
                     itemsData === defaultItemsData ? 
                     <p>Loading...</p>
                         :
-                    itemsData.map(function(item) {
-                        console.log(item)
+                    // console.log(itemsData),
+                    itemsData.map(function(item, itemIndex) {
                         return (
-                            <div key={item.itemid}>
-                                <p>{item.itemname}</p>
+                            <div className="item" key={item.itemid}>
+                                <div className="leftbox">
+                                    <p className="item-name">{item.itemname}</p>
+                                    {/* image will be here */}
+                                </div> 
+
+                                <div className="rightbox">
+                                    {
+                                        item.parts.map(function(section, partIndex) {
+                                            return (
+                                                <div className="rightbox-container" key={section.sectionname} >
+                                                    <div className="rightbox-section">
+                                                        <p className="rightbox-section-1">{section.sectionname}</p>
+                                                        <p className="rightbox-section-2">[{section.pricerange[0]} - {section.pricerange[1]}]</p>
+                                                        {
+                                                            section.dropdown === true ? <p className="rightbox-section-3" onClick={() => renderSection(itemIndex, partIndex)}> {section.extended === true ? "▼" : "▲"} </p> : <p className="rightbox-section-4"></p>
+                                                        }
+                                                    </div>
+
+                                                    <div className="rightbox-extended">
+                                                        {
+                                                            section.dropdown === true ? section.extended === true ? renderExtendedData(section, item, partIndex) : "" : ""
+                                                        }
+                                                    </div>
+                                                </div>
+                                                
+                                            )
+                                        })
+                                    }
+                                </div>
                             </div>
                         )
                     })
@@ -134,7 +197,7 @@ const Home = function() {
 
     return (
         <div className="home">
-            <button onClick={fetchData}>Refresh</button>
+            <button className="refresh-button" onClick={fetchData}>Refresh</button>
             {/* render items */}
             {
                 renderData()
