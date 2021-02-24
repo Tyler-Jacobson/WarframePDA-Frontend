2a3161685ae249e9b16a1ecfed2bda24b0bc3a63
Tyler Jacobson <TylerJacobsonSE@gmail.com>
2021-02-23 16:49:32 -0800

    working on displaying data

--- src/App.js
+++ src/App.js
@@ -1,36 +1,17 @@
-import react, { useEffect, useState} from "react";
+import { useEffect, useState} from "react";
 import './styles/App.css';
 import dummyData from "./dummyData";
 
-import WarframeCard from "./Components/WarframeCard";
+
+import Home from "./Components/Home";
 
 
 function App() {
 
-  const [ framesData, setFramesData ] = useState([]);
-
-  // for debugging
-  console.log(dummyData);
-
-  const fetchData = function() {
-    // axios call for main data here
-    setFramesData(dummyData)// will be res.data
-  }
-
-  useEffect(() => {
-    fetchData();
-  })
-  
-
   return (
     <div className="App">
-      {
-        dummyData.map(iter => {
-          return (
-            <WarframeCard frameData={iter} />
-          )
-        })
-      }
+      
+      <Home />
     </div>
   );
 }
--- src/dummyData.js
+++ src/dummyData.js
@@ -1,160 +1,174 @@
 
 const dummyData = [
     {
-      "name": "equinox prime",
+      "name": "equinox",
       "id": 1,
       "image" : "https://warframe.market/static/assets/icons/en/equinox_prime_set.1163c3dad7a8ffce02af7d475fc0ccd4.png",
       "idata": {
         "blueprint-data": {
+            "part": "blueprint",
             "price-range": [
-                {"seller": "oBT2", price: 4}, 
-                {"seller": "lukefly3", price: 5},
-                {"seller": "xMotts", price: 5},
-                {"seller": "HellbornTR", price: 5},
-                {"seller": "-Onyx-BanCity2K35", price: 5},
+                {"seller": "oBT2", "price": 4}, 
+                {"seller": "lukefly3", "price": 5},
+                {"seller": "xMotts", "price": 5},
+                {"seller": "HellbornTR", "price": 5},
+                {"seller": "-Onyx-BanCity2K35", "price": 5},
                 ]
-    
         },
         "chassis-data": {
+            "part": "chassis",
             "price-range": [
-                {"seller": "Mr.IcE_BreatH", price: 1}, 
-                {"seller": "Tsuryn", price: 2},
-                {"seller": "xMotts", price: 2},
-                {"seller": "HellbornTR", price: 2},
-                {"seller": "-Onyx-BanCity2K35", price: 2},
+                {"seller": "Mr.IcE_BreatH", "price": 1}, 
+                {"seller": "Tsuryn", "price": 2},
+                {"seller": "xMotts", "price": 2},
+                {"seller": "HellbornTR", "price": 2},
+                {"seller": "-Onyx-BanCity2K35", "price": 2},
                 ]
         },
         "neuroptics-data": {
+            "part": "neuroptics",
             "price-range": [
-                {"seller": "Mr.IcE_BreatH", price: 5}, 
-                {"seller": "Tsuryn", price: 5},
-                {"seller": "xMotts", price: 5},
-                {"seller": "HellbornTR", price: 5},
-                {"seller": "-Onyx-BanCity2K35", price: 6},
+                {"seller": "Mr.IcE_BreatH", "price": 5}, 
+                {"seller": "Tsuryn", "price": 5},
+                {"seller": "xMotts", "price": 5},
+                {"seller": "HellbornTR", "price": 5},
+                {"seller": "-Onyx-BanCity2K35", "price": 6},
                 ]
         },
         "set-data": {
+            "part": "set",
             "price-range": [
-                {"seller": "Nesko92", price: 30}, 
-                {"seller": "--Flip--", price: 30},
-                {"seller": "Adol85", price: 30},
-                {"seller": "DistanCZ", price: 31},
-                {"seller": "WeebframePrime", price: 33},
+                {"seller": "Nesko92", "price": 30}, 
+                {"seller": "--Flip--", "price": 30},
+                {"seller": "Adol85", "price": 30},
+                {"seller": "DistanCZ", "price": 31},
+                {"seller": "WeebframePrime", "price": 33},
                 ]
         },
         "systems-data": {
+            "part": "systems",
             "price-range": [
-                {"seller": "Mr.IcE_BreatH", price: 13}, 
-                {"seller": "Tsuryn", price: 15},
-                {"seller": "xMotts", price: 15},
-                {"seller": "HellbornTR", price: 15},
-                {"seller": "-Onyx-BanCity2K35", price: 17},
+                {"seller": "Mr.IcE_BreatH", "price": 13}, 
+                {"seller": "Tsuryn", "price": 15},
+                {"seller": "xMotts", "price": 15},
+                {"seller": "HellbornTR", "price": 15},
+                {"seller": "-Onyx-BanCity2K35", "price": 17},
                 ]
         }
       }
     },
     {
-      "name": "saryn prime",
+      "name": "saryn",
       "id": 2,
       "image" : "https://warframe.market/static/assets/icons/en/saryn_prime_set.394094bef0383f954bd38d622bb075e5.png",
       "idata": {
         "blueprint-data": {
+            "part": "blueprint",
             "price-range": [
-                {"seller": "oBT2", price: 30}, 
-                {"seller": "lukefly3", price: 30},
-                {"seller": "xMotts", price: 35},
-                {"seller": "HellbornTR", price: 40},
-                {"seller": "-Onyx-BanCity2K35", price: 40},
+                {"seller": "oBT2", "price": 30}, 
+                {"seller": "lukefly3", "price": 30},
+                {"seller": "xMotts", "price": 35},
+                {"seller": "HellbornTR", "price": 40},
+                {"seller": "-Onyx-BanCity2K35", "price": 40},
                 ]
     
         },
         "chassis-data": {
+            "part": "chassis",
             "price-range": [
-                {"seller": "Mr.IcE_BreatH", price: 99}, 
-                {"seller": "Tsuryn", price: 99},
-                {"seller": "xMotts", price: 100},
-                {"seller": "HellbornTR", price: 100},
-                {"seller": "-Onyx-BanCity2K35", price: 100},
+                {"seller": "Mr.IcE_BreatH", "price": 99}, 
+                {"seller": "Tsuryn", "price": 99},
+                {"seller": "xMotts", "price": 100},
+                {"seller": "HellbornTR", "price": 100},
+                {"seller": "-Onyx-BanCity2K35", "price": 100},
                 ]
         },
         "neuroptics-data": {
+            "part": "neuroptics",
             "price-range": [
-                {"seller": "Mr.IcE_BreatH", price: 15}, 
-                {"seller": "Tsuryn", price: 15},
-                {"seller": "xMotts", price: 15},
-                {"seller": "HellbornTR", price: 15},
-                {"seller": "-Onyx-BanCity2K35", price: 15},
+                {"seller": "Mr.IcE_BreatH", "price": 15}, 
+                {"seller": "Tsuryn", "price": 15},
+                {"seller": "xMotts", "price": 15},
+                {"seller": "HellbornTR", "price": 15},
+                {"seller": "-Onyx-BanCity2K35", "price": 15},
                 ]
         },
         "set-data": {
+            "part": "set",
             "price-range": [
-                {"seller": "Nesko92", price: 170}, 
-                {"seller": "--Flip--", price: 174},
-                {"seller": "Adol85", price: 175},
-                {"seller": "DistanCZ", price: 175},
-                {"seller": "WeebframePrime", price: 175},
+                {"seller": "Nesko92", "price": 170}, 
+                {"seller": "--Flip--", "price": 174},
+                {"seller": "Adol85", "price": 175},
+                {"seller": "DistanCZ", "price": 175},
+                {"seller": "WeebframePrime", "price": 175},
                 ]
         },
         "systems-data": {
+            "part": "systems",
             "price-range": [
-                {"seller": "Mr.IcE_BreatH", price: 8}, 
-                {"seller": "Tsuryn", price: 8},
-                {"seller": "xMotts", price: 9},
-                {"seller": "HellbornTR", price: 9},
-                {"seller": "-Onyx-BanCity2K35", price: 9},
+                {"seller": "Mr.IcE_BreatH", "price": 8}, 
+                {"seller": "Tsuryn", "price": 8},
+                {"seller": "xMotts", "price": 9},
+                {"seller": "HellbornTR", "price": 9},
+                {"seller": "-Onyx-BanCity2K35", "price": 9},
                 ]
         }
       }
     },
     {
-      "name": "nezha prime",
+      "name": "nezha",
       "id": 3,
       "image" : "https://warframe.market/static/assets/icons/en/nezha_prime_set.728ade174c899f22c2637580babda4cc.png",
       "idata": {
         "blueprint-data": {
+            "part": "blueprint",
             "price-range": [
-                {"seller": "oBT2", price: 10}, 
-                {"seller": "lukefly3", price: 10},
-                {"seller": "xMotts", price: 10},
-                {"seller": "HellbornTR", price: 10},
-                {"seller": "-Onyx-BanCity2K35", price: 10},
+                {"seller": "oBT2", "price": 10}, 
+                {"seller": "lukefly3", "price": 10},
+                {"seller": "xMotts", "price": 10},
+                {"seller": "HellbornTR", "price": 10},
+                {"seller": "-Onyx-BanCity2K35", "price": 10},
                 ]
     
         },
         "chassis-data": {
+            "part": "chassis",
             "price-range": [
-                {"seller": "Mr.IcE_BreatH", price: 35}, 
-                {"seller": "Tsuryn", price: 35},
-                {"seller": "xMotts", price: 37},
-                {"seller": "HellbornTR", price: 40},
-                {"seller": "-Onyx-BanCity2K35", price: 40},
+                {"seller": "Mr.IcE_BreatH", "price": 35}, 
+                {"seller": "Tsuryn", "price": 35},
+                {"seller": "xMotts", "price": 37},
+                {"seller": "HellbornTR", "price": 40},
+                {"seller": "-Onyx-BanCity2K35", "price": 40},
                 ]
         },
         "neuroptics-data": {
+            "part": "neuroptics",
             "price-range": [
-                {"seller": "Mr.IcE_BreatH", price: 18}, 
-                {"seller": "Tsuryn", price: 19},
-                {"seller": "xMotts", price: 19},
-                {"seller": "HellbornTR", price: 20},
-                {"seller": "-Onyx-BanCity2K35", price: 20},
+                {"seller": "Mr.IcE_BreatH", "price": 18}, 
+                {"seller": "Tsuryn", "price": 19},
+                {"seller": "xMotts", "price": 19},
+                {"seller": "HellbornTR", "price": 20},
+                {"seller": "-Onyx-BanCity2K35", "price": 20},
                 ]
         },
         "set-data": {
+            "part": "set",
             "price-range": [
-                {"seller": "Nesko92", price: 85}, 
-                {"seller": "--Flip--", price: 87},
-                {"seller": "Adol85", price: 88},
-                {"seller": "DistanCZ", price: 88},
-                {"seller": "WeebframePrime", price: 89},
+                {"seller": "Nesko92", "price": 85}, 
+                {"seller": "--Flip--", "price": 87},
+                {"seller": "Adol85", "price": 88},
+                {"seller": "DistanCZ", "price": 88},
+                {"seller": "WeebframePrime", "price": 89},
                 ]
         },
         "systems-data": {
+            "part": "systems",
             "price-range": [
-                {"seller": "Mr.IcE_BreatH", price: 8}, 
-                {"seller": "Tsuryn", price: 9},
-                {"seller": "xMotts", price: 9},
-                {"seller": "HellbornTR", price: 9},
-                {"seller": "-Onyx-BanCity2K35", price: 9},
+                {"seller": "Mr.IcE_BreatH", "price": 8}, 
+                {"seller": "Tsuryn", "price": 9},
+                {"seller": "xMotts", "price": 9},
+                {"seller": "HellbornTR", "price": 9},
+                {"seller": "-Onyx-BanCity2K35", "price": 9},
                 ]
         }
       }
